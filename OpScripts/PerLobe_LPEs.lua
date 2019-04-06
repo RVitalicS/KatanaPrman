d8d4ee82457f0feb01aa4c839150660f120645ee
RVitalicS <46557916+RVitalicS@users.noreply.github.com>
2019-04-06 17:24:50 +0300

    Rewritten to define LPEs and outputs in single OpScript So no need to use ROD_merged.lua as next operation

--- OpScripts/PerLobe_LPEs.lua
+++ OpScripts/PerLobe_LPEs.lua
@@ -3,13 +3,33 @@
 Location: /root
 renderer: prman
 
-Add outputChannels attributes for PerLobe LPE workflow
+Add outputChannels attributes for PerLobe LPE workflow and create render output as multichanneled exr file
+
+Required attributes:
+    user.projectPath: (string) path where result render file will be saved
+    user.shotName: (string) frame numbered name ('Name_{:03d}'.format(frame) -> AttributeSet)
 
 ]]
 
 
+-- add 'Ci' and 'a' channels
+Interface.SetAttr('prmanGlobalStatements.outputChannels.Ci.type', StringAttribute("varying color"))
+Interface.SetAttr('prmanGlobalStatements.outputChannels.Ci.name', StringAttribute("Ci"))
+
+Interface.SetAttr('prmanGlobalStatements.outputChannels.a.type', StringAttribute("varying float"))
+Interface.SetAttr('prmanGlobalStatements.outputChannels.a.name', StringAttribute("a"))
+
+
+-- global variable that collect all defined here LPE outputChannels as a string
+-- and will be used to adjust renderSettings.output attribute
+channels = 'Ci,a'
+
+
 function PrmanOutputChannelDefine (name, lpe)
     --[[ Works the same way as the PrmanOutputChannelDefine node ]]
+
+    -- add current LPE channel to global variable
+    channels = channels .. ',' .. name
 
     -- add two attributes grouped by input name
     -- these two attributes create base of outputChannel
@@ -56,9 +76,24 @@ PrmanOutputChannelDefine("transmissiveGlassLobe", "color lpe:C<TS8>[DS]*[<L.>O]"
 PrmanOutputChannelDefine("emissive", "color lpe:C[<L.>O]")
 
 
--- separately of all add 'Ci' and 'a' channels to merge them with LPEs later
-Interface.SetAttr('prmanGlobalStatements.outputChannels.Ci.type', StringAttribute("varying color"))
-Interface.SetAttr('prmanGlobalStatements.outputChannels.Ci.name', StringAttribute("Ci"))
+-- get string value from added earlier userdefined attribute that contains path for render outputs
+local path_attribute = Interface.GetAttr('user.projectPath')
+local path_project = Attribute.GetStringValue(path_attribute, '')
 
-Interface.SetAttr('prmanGlobalStatements.outputChannels.a.type', StringAttribute("varying float"))
-Interface.SetAttr('prmanGlobalStatements.outputChannels.a.name', StringAttribute("a"))
+-- get string value from added earlier userdefined attribute that contains name of the current shot
+local name_attribute = Interface.GetAttr('user.shotName')
+local name = Attribute.GetStringValue(name_attribute, '')
+
+-- create full path string to save multichanneled exr file
+local path = pystring.os.path.join(path_project, string.format ("%s_perLobeWF.exr", name) )
+
+
+
+-- Create one render output for all defined here LPE outputChannels
+
+-- add 'name' and 'raw' type parameters
+-- switch location type to 'file' mode and set 'renderLocation' parameter
+Interface.SetAttr('renderSettings.outputs.perLobeWF.type', StringAttribute("raw"))
+Interface.SetAttr('renderSettings.outputs.perLobeWF.rendererSettings.channel', StringAttribute(string.format ("%s", channels)))
+Interface.SetAttr('renderSettings.outputs.perLobeWF.locationType', StringAttribute("file"))
+Interface.SetAttr('renderSettings.outputs.perLobeWF.locationSettings.renderLocation', StringAttribute(path))
