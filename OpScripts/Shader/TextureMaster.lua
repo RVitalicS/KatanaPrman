b6e342e3a129802af910499429a5bb10f124180c
RVitalicS <46557916+RVitalicS@users.noreply.github.com>
2019-05-21 22:16:58 +0300

    Adjust script to have access read imported tags

---
+++ OpScripts/Shader/TextureMaster.lua
@@ -0,0 +1,75 @@
+--[[
+
+Location: /root/world/geo/asset//*{@type == "polymesh"} /root/world/geo/asset//*{@type == "subdmesh"}
+renderer: prman
+
+Find shader parameter tags, read values of those attributes
+and set texture attributes that will be read dynamically while render
+
+Otherwise set texture attributes with default values
+
+Required User Parameters:
+    user.texturesPath: (string) path to right textures
+    user.defaultsPath: (string) path to default textures
+
+]]
+
+
+
+function SetTexture(attr, path)
+    --[[
+    Creates attribute with texture path value
+
+    Arguments:
+        attr (string): attribute name that will be used in {attr:textures.xxx} syntax
+        path (string): path to texture file
+    ]]
+
+    Interface.SetAttr(string.format('textures.%s', attr), StringAttribute(path))
+
+end
+
+
+
+-- possible tags
+local shader_parameters = {
+    'diffuseColor',
+    'primSpecEdgeColor',
+    'primSpecRefractionIndex',
+    'primSpecExtinctionCoefficient',
+    'primSpecRoughness',
+    'normal',
+    'bump'}
+
+
+
+-- get path strings from user defined 'texturesPath' and 'defaultsPath' parameters
+local texturesPath = Attribute.GetStringValue(Interface.GetOpArg('user.texturesPath'), '')
+local defaultsPath = Attribute.GetStringValue(Interface.GetOpArg('user.defaultsPath'), '')
+
+
+
+-- look for all possible shader parameter tag
+for i=1, #shader_parameters do
+
+    -- get attribute name
+    local attr = shader_parameters[i]
+
+
+    -- if there was found current tag
+    -- create full path to texture file and set parameter
+    local shader_parameter = Interface.GetAttr(string.format('geometry.arbitrary.%s.value', attr))
+    if shader_parameter then
+
+        local path = texturesPath .. string.format('/%s', Attribute.GetStringValue(shader_parameter,''))
+        SetTexture(attr, path)
+
+
+    -- if there wasn't found current tag
+    -- create full path to default texture file and set parameter
+    else
+        local path = defaultsPath .. string.format('/%s.tex', attr)
+        SetTexture(attr, path)
+
+    end
+end
